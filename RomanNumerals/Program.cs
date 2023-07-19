4f7f86d0b473f375e7796d8211be396fec1d6451
Rob Reckless <rob.reckless@halfords.co.uk>
2023-07-19 15:35:00 +0100

    Initial

---
+++ RomanNumerals/Program.cs
@@ -0,0 +1,27 @@
+﻿using RomanNumerals;
+
+string passedValue = args.Length > 0 ? args[0].ToString() : "";
+
+if (passedValue == "")
+{
+    Console.WriteLine("Please enter an integer or Roman Numeral to convert:");
+
+    var response = Console.ReadLine();
+    if (response != null) passedValue = response;
+}
+
+if (passedValue =="")
+{
+    Console.WriteLine("Nothing to convert!");
+    return;
+}
+
+RomanNumeralConverter rnc = new RomanNumeralConverter();
+
+try
+{
+    int value = int.Parse(passedValue);
+    Console.WriteLine(rnc.ConvertToRomanNumerals(value));
+} catch {
+    Console.WriteLine(rnc.ConvertFromRomanNumerals(passedValue));
+}
---
+++ RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        public string ConvertToRomanNumerals(int value)
+        {
+            /* Task 1
+
+                Write a function that converts an integer (up to a max value of 3,999) into Roman Numerals
+    
+                I = 1
+                V = 5
+                X = 10
+                L = 50
+                C = 100
+                D = 500
+                M = 1000
+                
+            Example:
+                
+                2022 = MMXXII
+                3999 = MMMCMXCIX
+            
+            */
+            return "TODO";
+        }
+
+        public int ConvertFromRomanNumerals(string numeral)
+        {
+            /* Optional Task 2
+               
+                Write a function that converts a Roman numeral string back to it's integer value.
+
+            Example:
+                
+                MMXXI = 2021
+                MMMCMXCIX = 3999
+             */
+            return 0;
+        }
+    }
+}
